1e9607be9d2015165cf3537713f3d3d4984d1024
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-05-29 06:36:28 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,17 +4,31 @@
 
 <!-- Write content for each page here -->
 <h1>タスク新規作成ページ</h1>
+<div class="row">
+    <div class="col-xs-12">
+    <div class="col-sm-offset-2 col-sm-8">
+    <div class="col-md-offset-2 col-md-8">
+    <div class="col-lg-offset-3 col-md-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
+    <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+    </div>    
+    
+    <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    </div>    
 
-        {!! Form::submit('投稿') !!}
+        {!! Form::submit('投稿', ['class' => 'btn btn primary']) !!}
 
     {!! Form::close() !!}
+    </div>
+    </div>
+    </div>
+    </div>
+</div>
 
 @endsection
