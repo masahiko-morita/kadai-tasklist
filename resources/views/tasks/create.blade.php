9837ea2392afdd40ec1faf1465fb085ef3e6a815
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-05-30 00:26:57 +0000

    bootstrap design tasklist

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,10 +5,7 @@
 <!-- Write content for each page here -->
 <h1>タスク新規作成ページ</h1>
 <div class="row">
-    <div class="col-xs-12">
-    <div class="col-sm-offset-2 col-sm-8">
-    <div class="col-md-offset-2 col-md-8">
-    <div class="col-lg-offset-3 col-md-6">
+    <div class="col-xs-12","col-sm-offset-2 col-sm-8","col-md-offset-2 col-md-8","col-lg-offset-3 col-md-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
@@ -25,9 +22,6 @@
         {!! Form::submit('投稿', ['class' => 'btn btn primary']) !!}
 
     {!! Form::close() !!}
-    </div>
-    </div>
-    </div>
     </div>
 </div>
 
