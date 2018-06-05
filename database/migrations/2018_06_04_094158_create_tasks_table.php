3f9fe06fd9958218b7020a20587ab857203196d2
masahiko.morita <masahiko.a.morita@rakuten.com>
2018-06-05 02:05:04 +0000

    88

--- database/migrations/2018_06_04_094158_create_tasks_table.php
+++ database/migrations/2018_06_04_094158_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable88888 extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_06_04_094801_add_status_to_tasks_table.php
+++ database/migrations/2018_06_04_094801_add_status_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable8888 extends Migration
+class AddStatusToTasksTable extends Migration
 {
     /**
      * Run the migrations.
